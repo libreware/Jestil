4272eddfd44b3e2b69ea93c466cd975f8698b1c3
Ben <ben@juliand.co.uk>
2016-02-19 17:30:10 +0000

    Added activities index
    In order to reuse it in the activity factory to decouple the activities
    from the runner.

---
+++ activities/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    JMSListen: require('./JMS/JMSListen'),
+    JMSSend: require('./JMS/JMSSend'),
+    JMSAcknowledge: require('./JMS/JMSAcknowledge'),
+    Log: require('./General/Log'),
+    MapTransform: require('./General/MapTransform')
+};
