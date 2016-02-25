6fe602b046f9e1da56e346d39475f3a961911619
Ben <ben@juliand.co.uk>
2016-02-25 09:54:54 +0000

    Added an example for creating a job

--- examples/creatingAJob.js
+++ examples/creatingAJob.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let Builder = require('../lib/builder/builder.js');
+let TBuilder = require('../lib/builder/lib/transformBuilder.js');
 let connection = {port: 61613, host: '127.0.0.1'};
 let destination = '/queue/test';
 
@@ -14,17 +15,14 @@ let Listener = Job.createActivity('JMSListen', {
 //TODO make the mapping creation more user friendly / less technical
 let Book2Pivot = Job.createActivity('MapTransform', {
     name: 'Book 2 Pivot',
-    input: {_jsonPath: '$.JMSListen1.content'}, //TODO make more user friendly
+    input: TBuilder.path(`$.${Listener.id}.content`), //TODO make more user friendly
     params: {
         mapping: {
             myBooks: {
-                authors: {_jsonPath: '$.store.book[*].author'},
-                titles: {_jsonPath: '$.store.book[*].title'},
-                totalPrice: {
-                    _jsonPath: '$.store.book[*].price',
-                    _transformation: '(arr) => arr.reduce((pre, curr) => pre + curr)'
-                },
-                favouriteBook: {_jsonPath: '$..book[3]'}
+                authors: TBuilder.path('$.store.book[*].author'),
+                titles: TBuilder.path('$.store.book[*].title'),
+                totalPrice: TBuilder.transform(TBuilder.path('$.store.book[*].price'), (arr) => arr.reduce((pre, curr) => pre + curr)),
+                favouriteBook: TBuilder.path('$..book[3]')
             }
         }
     }
@@ -33,16 +31,13 @@ let Book2Pivot = Job.createActivity('MapTransform', {
 let LogResult = Job.createActivity('Log', {
     name: 'Log result',
     input: {
-        result: {
-            _jsonPath: '$.Map_Transform1.myBooks.totalPrice',
-            _transformation: '(res) => parseInt(res) + 1000'
-        }
+        result: TBuilder.transform(TBuilder.path(`$.${Book2Pivot.id}.myBooks.totalPrice`), (res) => parseInt(res) + 1000)
     }
 });
 
 let ACK = Job.createActivity('JMSAcknowledge', {
     name: 'ACK',
-    input: {_jsonPath: '$.JMSListen1'},
+    input: TBuilder.path(`$.${Listener.id}`),
     params: {target: Listener.id}
 });
 
